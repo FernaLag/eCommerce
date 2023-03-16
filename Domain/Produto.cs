d7dfd31d12662ccf4d08fe2bd7854bf946632753
Xande <alexandre@cprautomacao.com.br>
2023-03-16 19:40:18 -0300

    Alteracao Produto

---
+++ Domain/Produto.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain
+{
+    public class Produto
+    {
+        public string Nome {get;set;}
+        public string Tamanho {get;set;}
+        public decimal Preco {get;set;}
+        public int QuantidadeDisponivel {get;set;}
+
+   
+    }
+
+}
