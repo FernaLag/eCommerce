0ce0f3b3db85259db6a4f47db84500e1770739a6
xandao06 <alexandre@cprautomacao.com.br>
2023-04-11 20:03:29 -0300

    a

---
+++ src/UI/Controllers/ProdutoController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+
+namespace UI.Controllers
+{
+    public class ProdutoController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+         [HttpPost]
+         [ValidateAntiForgeryToken]
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
