b513a6208adfa21b0848474c15fca35186115d04
GabCabe <gabbriellaguna@gmail.com>
2023-04-11 20:04:42 -0300

    pedidos

---
+++ src/UI/Controllers/EnderecoController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UI.Controllers
+{
+    public class EnderecoController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+
+        }
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+    }
+}
---
+++ src/UI/Views/Endereco/index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UI.Views.Endereco
+{
+    public class indexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
