b3c2cd02dd4029971133430332c18b44d7703cc1
xandao06 <alexandre@cprautomacao.com.br>
2023-04-13 20:20:09 -0300

    MVP

---
+++ src/UI/Views/Produto/Index1.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UI.Views.Produto
+{
+    public class Index1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
